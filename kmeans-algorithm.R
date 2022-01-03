918d330f07f62c4a5459e2441510dd00dd6f870d
Rod Morgado <rodrigo@zooft.company>
2022-01-03 15:13:07 -0800

    plot using a scatterplot the variable x and coloring km.out$clusteplot

--- kmeans-algorithm.R
+++ kmeans-algorithm.R
@@ -3,3 +3,12 @@ km.out <-kmeans(x, centers = 3, nstart = 20)
 
 # Inspect the result
 summary(km.out)
+
+# Print the cluster membership component of the model
+km.out$cluster
+
+# Print the km.out object
+km.out
+
+# Scatter plot of x
+plot(x, col = km.out$cluster, main = "k-means with 3 clusters", xlab = "", ylab= "")
