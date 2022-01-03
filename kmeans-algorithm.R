2315c23e412c236f84ca1dd0c60bcc062e85094a
Rod Morgado <rodrigo@zooft.company>
2022-01-03 15:48:39 -0800

    running a kmeans algorithm, finding the right number of cluster and scatterplot defense vs. speed

--- kmeans-algorithm.R
+++ kmeans-algorithm.R
@@ -47,3 +47,34 @@ plot(1:15, wss, type = "b",
 # Set k equal to the number of clusters corresponding to the elbow location
 
 k <- 2
+
+# Initialize total within sum of squares error: wss
+wss <- 0
+
+# Look over 1 to 15 possible clusters
+for (i in 1:15) {
+  # Fit the model: km.out
+  km.out <- kmeans(pokemon, centers = i, nstart = 20, iter.max = 50)
+  # Save the within cluster sum of squares
+  wss[i] <- km.out$tot.withinss
+}
+
+# Produce a scree plot
+plot(1:15, wss, type = "b", 
+     xlab = "Number of Clusters", 
+     ylab = "Within groups sum of squares")
+
+# Select number of clusters
+k <- 3
+
+# Build model with k clusters: km.out
+km.out <- kmeans(pokemon, centers = k, nstart = 20, iter.max = 50)
+
+# View the resulting model
+km.out
+
+# Plot of Defense vs. Speed by cluster membership
+plot(pokemon[, c("Defense", "Speed")],
+     col = km.out$cluster,
+     main = paste("k-means clustering of Pokemon with", k, "clusters"),
+     xlab = "Defense", ylab = "Speed")
