40cda4dd397146c714216b959667ddae19c1ba1e
Rod Morgado <rodrigo@zooft.company>
2022-01-03 15:23:17 -0800

    plotting six different kmeans models to see how randomly the clusters are seleted the first time

--- kmeans-algorithm.R
+++ kmeans-algorithm.R
@@ -12,3 +12,19 @@ km.out
 
 # Scatter plot of x
 plot(x, col = km.out$cluster, main = "k-means with 3 clusters", xlab = "", ylab= "")
+
+# Set up 2 x 3 plotting grid
+par(mfrow = c(2, 3))
+
+# Set seed
+set.seed(1)
+
+for(i in 1:6) {
+  # Run kmeans() on x with three clusters and one start
+  km.out <- kmeans(x, centers = 3, nstart = 1)
+  
+  # Plot clusters
+  plot(x, col = km.out$cluster, 
+       main = km.out$tot.withinss, 
+       xlab = "", ylab = "")
+}
