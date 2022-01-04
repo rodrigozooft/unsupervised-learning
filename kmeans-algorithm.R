b19e32728105df0b9287a632fe2cf57475b79a4a
Rod Morgado <rodrigo@zooft.company>
2022-01-03 16:10:47 -0800

    using cutree to cut (or stop) the clustering process by height or by the number of clusters produced:w

--- kmeans-algorithm.R
+++ kmeans-algorithm.R
@@ -84,3 +84,9 @@ hclust.out <- hclust(d = dist(x))
 
 # Inspect the result
 summary(hclust.out)
+
+# Cut by height
+cutree(hclust.out, h = 7)
+
+# Cut by number of clusters
+cutree(hclust.out, k = 3)
