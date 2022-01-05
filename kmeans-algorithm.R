df1f660b8f94d71813876aed1748c3393c04fc0a
Rod Morgado <rodrigo@zooft.company>
2022-01-04 16:14:35 -0800

    plotting the variance of the components to get the number of components that explain the data

--- kmeans-algorithm.R
+++ kmeans-algorithm.R
@@ -138,3 +138,13 @@ pr.var <- pr.out$sdev ^ 2
 
 # Variance explained by each principal component: pve
 pve <- pr.var / sum(pr.var)
+
+# Plot variance explained for each principal component
+plot(pve, xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
+
+# Plot cumulative proportion of variance explained
+plot(cumsum(pve), xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
